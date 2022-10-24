764dd7f043661431cc201cf55b037be476d4fc7a
Jose Beneyto <sepen@crux.nu>
2022-10-24 03:48:54 +0200

    Add RELEASE_TAR_FILE to devices

--- devices/raspberrypi3.mk
+++ devices/raspberrypi3.mk
@@ -1,2 +1,3 @@
 CFLAGS = -O2 -pipe -march=armv8-a+crc -mabi=lp64 -mlittle-endian
 COLLECTIONS = raspberrypi3-arm64 core-arm64 core
+RELEASE_TAR_FILE = crux-arm-$(CRUX_ARM_VERSION)-aarch64-raspberrypi3.rootfs.tar.xz
