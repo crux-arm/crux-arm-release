fba3154b0d561e3c6321d2de98251acdd98cdfb2
Victor Martinez <pitillo@crfux-arm.nu>
2022-11-04 10:04:44 +0100

    Added samsung-chromebook.mk optimization file

---
+++ devices/samsung-chromebook.mk
@@ -0,0 +1,3 @@
+CFLAGS= -O2 -pipe -mfloat-abi=hard -march=armv7-a -mtune=cortex-a15 -mfpu=neon
+COLLECTIONS = samsung-chromebook-arm core-arm core
+RELEASE_TAR_FILE = crux-arm-$(CRUX_ARM_VERSION)-samsung-chromebook.rootfs.tar.xz
