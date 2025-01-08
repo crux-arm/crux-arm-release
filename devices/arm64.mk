27597f10cc61a0ed967c9610fd015de8fc9c55fd
Jose Beneyto <sepen@crux.nu>
2025-01-08 22:10:51 +0100

    Improve documentation in arm64.mk and armhf.mk

--- devices/arm64.mk
+++ devices/arm64.mk
@@ -1,7 +1,12 @@
-# This is a generic optimization for aarch64-linux-gnu
+# This is a generic optimization for arm64
 # For 64-bit based devices, targeting ARMv8 architecture
 
-# Generic compiler options for aarch64-linux-gnu
+# Generic compiler options for arm64 (aarch64-unknown-linux-gnu)
+#
+# -march=armv8-a  – Targets the ARMv8-A architecture, which is the most common
+#                   64-bit ARM architecture.
+# -fPIC           – Ensures Position Independent Code (PIC), which is necessary
+#                   for creating shared libraries on ARM64 systems.
 CFLAGS = -O2 -pipe -march=armv8-a -fPIC
 
 # Port collections required to build a generic arm64 release
--- devices/armhf.mk
+++ devices/armhf.mk
@@ -1,11 +1,18 @@
-# This is a generic optimization for arm-linux-gnueabihf
+# This is a generic optimization for armhf
 # For 32-bit based devices, using the hard-float version
 # of the "new" ABI (EABI), targeting ARMv7 and up
 
-# Generic compiler options for arm-linux-gnueabihf
+# Generic compiler flags for armhf (arm-unknown-linux-gnueabihf)
+#
+# -march=armv7-a   – Targets ARMv7-A architecture (generic for armhf).
+# -mfloat-abi=hard – Uses hardware floating-point ABI (required for armhf).
+# -mfpu=vfpv3-d16  – Configures the FPU to VFPv3 with 16 registers (common for ARMv7).
+#                    If the device supports NEON, you can use -mfpu=neon.
+# -fPIC            - Ensures Position Independent Code (PIC), which is necessary
+#                    for creating shared libraries on ARMv7 systems.
 CFLAGS = -O2 -pipe -march=armv7-a -mfloat-abi=hard -mfpu=vfpv3 -fPIC
 
-# Port collections required to build a generic arm release
+# Port collections required to build a generic armhf release
 COLLECTIONS = core-arm core
 
 # Release file
