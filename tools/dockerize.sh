5fdbf15678b7273f025e19445312b296bb515696
Jose Beneyto <sepen@crux.nu>
2025-01-08 20:15:28 +0100

    Major improvements to dockerize.sh script
    - OS detection and fix for Docker Desktop on macOS, which requires a case-sensitive filesystem on volumes.
    - Use of separate directories for mounting as volumes, thus avoiding a high rate of disk access operations and potential issues related to "too many open files..."

--- tools/dockerize.sh
+++ tools/dockerize.sh
@@ -1,30 +1,68 @@
 #!/usr/bin/env bash
 #
-# Run make targets inside docker containers
-# E.g. tools/dockerize.sh stage0
+# Run make commands inside a container
+# E.g. tools/dockerize.sh stage0 DEVICE_OPTIMIZATION=arm64
+#
 
 
+# Get make command and params
+MAKE_PARAMS="help"
+[ $# -ge 1 ] && MAKE_PARAMS="$*"
+
+# Device optimizations (see: README.md)
+DEVICE_OPTIMIZATION=${DEVICE_OPTIMIZATION:-arm64}
+
+# Get directory tree and setup paths
 TOOLS_DIR=$(cd "$(dirname "$0")" && pwd)
 BASE_DIR=$(cd "${TOOLS_DIR}"/../ && pwd)
 
-DEVICE_OPTIMIZATION=${DEVICE_OPTIMIZATION:-arm64}
+# Detect the host system and fix the necessary requirements if needed
+HOST_OS=$(uname -s)
+case "${HOST_OS}" in
+  "Darwin")
+    # Problem:
+    #   macOS uses a case-insensitive filesystem (APFS or HFS+), which can cause
+    #   issues when bind mounting directories into Docker containers.
+    # Fix:
+    # - Create a Case-Sensitive APFS Disk Container:
+    #   hdiutil create -type SPARSE -fs "Case-sensitive APFS" -size 10g -volname MyDisk mydisk.dmg
+    # - Mount the Disk Container:
+    #   hdiutil attach mydisk.dmg -mountpoint work/
+    # Important Note:
+    #  Docker-Desktop v4.37.1 (178610) didn't work and reported a 'mount read-only' issue
+    #  Docker-Desktop v4.34.3 (170107) has been tested and worked without problems
+    echo "Darwin macOS detected. Checking for mounting volume in ${BASE_DIR}/work"
+    df "${BASE_DIR}/work" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      echo "Mounting a case-sensitive volume to ${BASE_DIR}/work"
+      mkdir -p "${BASE_DIR}/work"
+      if [ ! -f "${BASE_DIR}/work.dmg.sparseimage" ]; then
+        hdiutil create -type SPARSE -fs "Case-sensitive APFS" -size 10g -volname "crux-arm-release-work" "${BASE_DIR}/work.dmg"
+      fi
+      hdiutil attach "${BASE_DIR}/work.dmg.sparseimage" -mountpoint "${BASE_DIR}/work" || exit 1
+    fi
+    ;;
+esac
+
+# Set workspace dir inside the container
+WORKSPACE_DIR="/crux-arm-release"
+
+# Docker builder image and platform
 DOCKER_IMAGE=${DOCKER_IMAGE:-sepen/crux:3.7-arm64-builder}
+DOCKER_PLATFORM=${DOCKER_PLATFORM:-linux/arm64}
 
-ULIMIT_VALUE=${ULIMIT_VALUE:-65536}
-# ulimit recommendations for Docker and Development
-# - Docker Desktop or intensive workloads:
-#   65536 to 200000 is common.
-# - Heavy builds or high I/O workloads:
-#   200000 to 400000 is reasonable.
-# - Very high file descriptor usage (databases, large-scale apps):
-#   Values up to 524288 can be necessary.
-ulimit -n ${ULIMIT_VALUE}
-
-MAKE_PARAMS="bootstrap"
-[ $# -ge 1 ] && MAKE_PARAMS="$*"
-
-docker run -it -v "${BASE_DIR}":/crux-arm-release "${DOCKER_IMAGE}" bash -x -c "
-export DEVICE_OPTIMIZATION=${DEVICE_OPTIMIZATION}
-cd /crux-arm-release
-make ${MAKE_PARAMS}
+# Run the docker command and bind some directories
+# Avoid the whole directory tree of this project to ${WORKSPACE_DIR} since
+# this will end up with "too many open files" when managing large docker volumes
+docker run --init --rm \
+    --platform "${DOCKER_PLATFORM}" \
+    -v "${BASE_DIR}/Makefile":${WORKSPACE_DIR}/Makefile \
+    -v "${BASE_DIR}/ports":${WORKSPACE_DIR}/ports \
+    -v "${BASE_DIR}/devices":${WORKSPACE_DIR}/devices \
+    -v "${BASE_DIR}/sources":${WORKSPACE_DIR}/sources \
+    -v "${BASE_DIR}/packages":${WORKSPACE_DIR}/packages \
+    -v "${BASE_DIR}/work":${WORKSPACE_DIR}/work \
+    "${DOCKER_IMAGE}" bash -x -c "
+cd ${WORKSPACE_DIR}
+make V=1 ${MAKE_PARAMS} DEVICE_OPTIMIZATION=${DEVICE_OPTIMIZATION}
 "
