17cc856376edd47127b3e6ddfcf028a618a74488
Tim Biermann <tbier@posteo.de>
2025-02-09 15:22:45 +0100

    cherry-picked 36bda8c98fc48a4576cd962745effac9abcfdde8 (#36)

--- devices/raspberrypi5.mk
+++ devices/raspberrypi5.mk
@@ -1,3 +1,3 @@
 CRUX_ARM_ARCH = arm64
-CFLAGS = -O2 -mlittle-endian -mabi=lp64 -march=armv8.2-a+crypto+fp16+rcpc+dotprod -fasynchronous-unwind-tables
+CFLAGS = -O2 -mlittle-endian -march=armv8.2-a+crypto+fp16+rcpc+dotprod -fasynchronous-unwind-tables
 COLLECTIONS = raspberrypi5-arm64 core-arm64 core
